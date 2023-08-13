1926ec545ba5577fdb5516891508634684c1bb03
emrekandaz <emrekandazoglu52@gmail.com>
2023-08-13 17:16:06 +0300

    uptade 3

--- main.py
+++ main.py
@@ -7,7 +7,19 @@ app = FastAPI()
 def index():
     return {'data':'blog list'}
 
+@app.get('/blog/unpublished') # we have to put static route above dynamic route
+def unpublished():
+    #fetch blog with id=id
+    return {'data':'unpublished'}
+
 @app.get('/blog/{id}')
-def show():
+def show(id: int): # we changed type of id
     #fetch blog with id=id
     return {'data':id}
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments of blog with id=id
+    return  {'data':{'1':'hello','2':'hello'}}
